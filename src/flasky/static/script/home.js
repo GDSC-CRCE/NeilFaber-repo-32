d54cdfef01fe938051c3315b21852a39aa92454a
Gabbar <147980619+Gabbar-v7@users.noreply.github.com>
2024-10-12 20:39:07 +0530

    Mobile Cart icon fixes

--- src/flasky/static/script/home.js
+++ src/flasky/static/script/home.js
@@ -1,9 +1,11 @@
 var cartNumber = document.getElementById("cart-number");
+var mobileCart = document.getElementById("mobile-cart");
 
 async function intialise() {
   var cart = JSON.parse(localStorage.getItem("cart"));
   if (cart) {
     cartNumber.innerHTML = cart.length;
+    mobileCart.innerHTML = cart.length;
   }
 }
 
